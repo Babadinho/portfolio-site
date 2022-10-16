3f17e0184dfe9182d6fec73fe131ec5949b6bf78
Donald Iloekwe <donytex4u@gmail.com>
2022-10-16 01:55:26 +0100

    added light mode toggle

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ for (let i = 0; i < side_menu_list.length; i++) {
     side_menu.style.right = '-200px';
   });
 }
+
+// light mode and save to local storage
+window.onload = function () {
+  if (localStorage.lightMode == 'true') {
+    document.body.classList.toggle('body-light');
+    document.getElementById('checkbox').checked = true;
+  } else {
+    document.body.classList.toggle('');
+  }
+};
+document.getElementById('checkbox').addEventListener('change', () => {
+  document.body.classList.toggle('body-light');
+  localStorage.lightMode = localStorage.lightMode == 'true' ? 'false' : 'true';
+});
