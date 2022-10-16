ba8c86dc3bff497978fb1fe4bc92619e81574836
Donald Iloekwe <donytex4u@gmail.com>
2022-10-16 11:59:02 +0100

    header pointer and css fix

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let side_menu = document.getElementById('side_menu');
+let header = document.getElementById('header');
 let side_menu_list = document.querySelectorAll('.side_menu')[0].children;
+
+// light mode and save to local storage
+window.onload = function () {
+  if (localStorage.lightMode == 'true') {
+    document.body.classList.toggle('body-light');
+    document.getElementById('checkbox').checked = true;
+  } else {
+    document.body.classList.toggle('body');
+  }
+};
+document.getElementById('checkbox').addEventListener('change', () => {
+  document.body.classList.toggle('body-light');
+  localStorage.lightMode = localStorage.lightMode == 'true' ? 'false' : 'true';
+});
+
+let pointer = document.createElement('span');
+pointer.innerHTML = `<a href='#skills' class='pointer'>╲╱<a>`;
+
+header.appendChild(pointer);
 
 const open_menu = () => {
   side_menu.style.right = '0';
@@ -22,18 +42,4 @@ window.addEventListener('mouseup', function (e) {
   if (e.target != side_menu && e.target.parentNode != side_menu) {
     side_menu.style.right = '-20rem';
   }
-});
-
-// light mode and save to local storage
-window.onload = function () {
-  if (localStorage.lightMode == 'true') {
-    document.body.classList.toggle('body-light');
-    document.getElementById('checkbox').checked = true;
-  } else {
-    document.body.classList.toggle('body');
-  }
-};
-document.getElementById('checkbox').addEventListener('change', () => {
-  document.body.classList.toggle('body-light');
-  localStorage.lightMode = localStorage.lightMode == 'true' ? 'false' : 'true';
 });
